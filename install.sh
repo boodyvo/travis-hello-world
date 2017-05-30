3060a93d8d3955eea35dd204a2c4558b4f40d3a5
Vladyslav Budichenko <vladyslav.budichenko@bitfury.com>
2017-05-30 13:57:44 +0300

    Changes

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ printf 'export PATH=$PATH:$GOPATH/bin\n' >> ~/.bashrc
 source ~/.bashrc
 echo $GOVERSION
 echo $PATH
+export GOVERSION=1.7.1
+export GOPATH=$HOME/golang-source
+export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+echo $GOVERSION
+echo $PATH
 wget https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz && \
 tar -C /usr/local -xzf go${GOVERSION}.linux-amd64.tar.gz&& \
 rm go${GOVERSION}.linux-amd64.tar.gz
