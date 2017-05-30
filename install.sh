76a3e4bcff20d9e1584ac95480a154c59f9b670c
Vladyslav Budichenko <vladyslav.budichenko@bitfury.com>
2017-05-30 13:38:08 +0300

    Changes

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-apt-get install nano
+printf 'export GOVERSION=1.7.1\n' >> ~/.bashrc
+printf 'export GOPATH=$HOME/golang-source\n' >> ~/.bashrc
+printf 'export PATH=$PATH:/usr/local/go/bin\n' >> ~/.bashrc
+printf 'export PATH=$PATH:$GOPATH/bin\n' >> ~/.bashrc
+source ~/.bashrc
+wget https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz && \
+tar -C /usr/local -xzf go${GOVERSION}.linux-amd64.tar.gz&& \
+rm go${GOVERSION}.linux-amd64.tar.gz
