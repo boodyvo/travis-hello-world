81db743a76b03c4d9c4170f20cec917e32c87877
Vladyslav Budichenko <vladyslav.budichenko@bitfury.com>
2017-05-30 14:08:26 +0300

    Changes

--- install.sh
+++ install.sh
@@ -15,3 +15,22 @@ echo $PATH
 wget https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz && \
 tar -C /usr/local -xzf go${GOVERSION}.linux-amd64.tar.gz&& \
 rm go${GOVERSION}.linux-amd64.tar.gz
+
+apt-get --yes install git
+
+go get -u github.com/Masterminds/glide
+
+wget https://github.com/google/protobuf/releases/download/v3.0.2/protoc-3.0.2-linux-x86_64.zip
+unzip protoc-3.0.2-linux-x86_64.zip -d /usr/local
+rm protoc-3.0.2-linux-x86_64.zip
+
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+git clone https://github.com/BitfuryLightning/lnd-simnet-env $GOPATH/src/github.com/BitfuryLightning/lnd-simnet-env
+cd $GOPATH/src/github.com/BitfuryLightning/lnd-simnet-env
+git checkout -b python-grpc-client
+git pull origin python-grpc-client
+glide install
+./build.sh
